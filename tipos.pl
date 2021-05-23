563ac1f7beb4d93ab53c75a77e28e0b801fb3d1d
WendsonMagalhaes <wendson.silva@ccc.ufcg.edu.br>
2021-05-22 21:05:47 -0300

    Atualização do Tabuleiro

--- tipos.pl
+++ tipos.pl
@@ -1,18 +1,18 @@
-branco(pB, 'Peão branco').
-branco(tB, 'Torre branca').
-branco(cB, 'Cavalo branco').
-branco(bB, 'Bispo branco').
-branco(dB, 'Dama branca').
-branco(rB, 'Rei branco').
+branco('\u265F', 'Peão branco').
+branco('\u265C', 'Torre branca').
+branco('\u265E', 'Cavalo branco').
+branco('\u265D', 'Bispo branco').
+branco('\u265B', 'Dama branca').
+branco('\u265A', 'Rei branco').
 
-preto(pP, 'Peão preto').
-preto(tP, 'Torre preta').
-preto(cP, 'Cavalo preto').
-preto(bP, 'Bispo preto').
-preto(dP, 'Dama preta').
-preto(rP, 'Rei preto').
+preto('\u2659', 'Peão preto').
+preto('\u2656', 'Torre preta').
+preto('\u2658', 'Cavalo preto').
+preto('\u2657', 'Bispo preto').
+preto('\u2655', 'Dama preta').
+preto('\u2654', 'Rei preto').
 
-vazio(vZ, 'Espaço vazio').
+vazio('\u25A1', 'Espaço vazio').
 
-pecasBrancas(R) :- R = [pB, tB, cB, bB, dB, rB].
-pecasPretas(R) :- R = [pP, tP, cP, bP, dP, rP].
+pecasBrancas(R) :- R = ['\u265F','\u265C','\u265E', '\u265D', '\u265B', '\u265A'].
+pecasPretas(R) :- R = ['\u2659', '\u2656', '\u2658', '\u2657', '\u2655', '\u2654'].
